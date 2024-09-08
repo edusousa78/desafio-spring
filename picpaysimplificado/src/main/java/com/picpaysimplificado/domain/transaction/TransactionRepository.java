72b95d0801209e8caed5c5379287e478eaa17ce2
edusousa78 <sousa.ej@gmail.com>
2024-09-08 12:23:11 -0300

    Classe repository ana

---
+++ picpaysimplificado/src/main/java/com/picpaysimplificado/domain/transaction/TransactionRepository.java
@@ -0,0 +1,9 @@
+package com.picpaysimplificado.domain.transaction;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long>{
+
+    
+
+}
